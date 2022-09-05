cca09e99cf0d8106633a918584902105df58d4d5
oscarsaravia <oscarsaraviard0013@gmail.com>
2022-09-05 15:49:36 -0600

    Finished exercise 2

---
+++ ejercicio2.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import random
+
+# VARIABLE ALEATORIA: NUMERO DE CARAS AL TIRAR 3 MONEDAS DE UNA MONEDA JUSTA
+probability_distribution = {
+  0: 1/8,
+  1: 3/8,
+  2: 3/8,
+  3: 1/8,
+}
+
+def generate_values(n):
+  values = []
+  for _ in range(n):
+    number_of_heads = random.randint(0, 3)
+    probability = probability_distribution[number_of_heads]
+    values.append(probability)
+  
+  plt.title('Ejercicio 2')
+  plt.xlabel('Probabilidad')
+  plt.ylabel('Total de eventos')
+  plt.hist(values, color='#369399')
+  plt.show()
+
+
+generate_values(100)
+
